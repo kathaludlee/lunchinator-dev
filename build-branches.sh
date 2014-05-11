d78703011b0c3a3e641d99d2bda6375f0606798e
Hannes Rauhe <hannes@scitivity.net>
2014-05-11 18:37:43 +0200

    modified build script

---
+++ build-branches.sh
@@ -0,0 +1,43 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $DIR
+
+git clone https://github.com/hannesrauhe/lunchinator.git
+
+branches=(master nightly)
+
+for branch in "${branches[@]}"
+do
+  LAST_HASH="HEAD^"
+  if [ -e last_hash_${branch} ]
+  then
+    LAST_HASH=$(cat last_hash_${branch})
+  fi
+  
+  pushd lunchinator
+  git checkout $branch
+  git pull
+
+  THIS_HASH="$(git rev-parse HEAD)"
+
+  if [ $LAST_HASH == $THIS_HASH ]
+  then
+    popd 
+    echo "No new version in git for $branch"
+  else
+    VERSION="$(git describe --tags --abbrev=0).$(git rev-list HEAD --count)"
+    echo $VERSION > version
+    git log $LAST_HASH..HEAD --oneline --no-merges > changelog
+    popd
+
+    #echo $VERSION
+    ./make_deb.sh
+    ./make_deb.sh --clean
+
+    echo $THIS_HASH > last_hash_${branch}
+  fi
+
+  sleep 2
+done
+
+popd
---
+++ make_deb.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+function generate_changelog() {
+  cat ../../lunchinator/changelog |
+  while read line 
+  do
+    echo "$line"
+    dch -a "$line"
+  done
+  sed -i -e '/automatically created by stdeb/d' debian/changelog
+}
+
+dists=(trusty)
+
+args=$(getopt -l "publish,clean" -o "pc" -- "$@")
+
+if [ ! $? == 0 ]
+then
+  exit 1
+fi
+
+eval set -- "$args"
+
+PUBLISH=false
+
+while [ $# -ge 1 ]; do
+  case "$1" in
+    --)
+        # No more options left.
+        shift
+        break
+       ;;
+    -p|--publish)
+        PUBLISH=true
+        shift
+        ;;
+    -c|--clean)
+        rm -rf deb_*/ *.log dist
+        exit 0
+        ;;
+    -h)
+        echo "Use with -p|--publish to publish to Launchpad immediately."
+        exit 0
+        ;;
+  esac
+
+  shift
+done
+
+if [ "$DEBFULLNAME" == "" ] || [ "$DEBEMAIL" == "" ]
+then
+  echo "Please export DEBFULLNAME and DEBEMAIL to your environment."
+  exit 1
+fi
+
+if ! type py2dsc &>/dev/null
+then
+  echo "Please install python-stdeb first, using pip. Do NOT install it via aptitude!"
+  exit 1
+fi
+
+if python -c "from stdeb.util import PYTHON_ALL_MIN_VERS; print PYTHON_ALL_MIN_VERS" &>/dev/null
+then
+  echo "Please uninstall python-stdeb via aptitude and install it via pip."
+  exit 1
+fi
+
+if ! type dch &>/dev/null
+then
+  echo "Please install devscripts first."
+  exit 1
+fi
+
+if ! type lintian &>/dev/null
+then
+  echo "Please install lintian first."
+  exit 1
+fi
+
+for dist in "${dists[@]}"
+do
+  echo -e "\e[00;31m***** Creating source package for ${dist} *****\e[00m"
+  export dist
+  rm -rf dist deb_${dist}
+  pushd lunchinator
+  export __lunchinator_branch=$(git rev-parse --abbrev-ref HEAD)
+  export __isubuntu=1 #make sure setup.py builds for ubuntu
+  python setup.py sdist --dist-dir=../dist
+  popd
+  py2dsc --suite=${dist} --dist-dir=deb_${dist} dist/Lunchinator*
+  pushd deb_${dist}/lunchinator-*
+  generate_changelog
+  debuild -S 2>&1 | tee ../../${dist}.log
+  if $PUBLISH
+  then
+    pushd ..
+    echo -e "\e[00;31m***** Publishing package for ${dist} *****\e[00m"
+    dput ppa:lunch-team/lunchinator lunchinator_*.changes
+    popd
+  fi
+  popd
+done
